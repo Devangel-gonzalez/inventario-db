b1b2596fa15d18c91b9ded690453ddcb8614f230
Angel Israel Gonzalez Torres <a222213581@unison.mx>
2026-04-26 15:48:24 -0700

    ormlite

--- src/main/java/mx/unison/Usuario.java
+++ src/main/java/mx/unison/Usuario.java
@@ -1,6 +1,19 @@
 package mx.unison;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "usuarios")
 public class Usuario {
+    @DatabaseField(id = true)
     public String nombre;
-    public String rol;
+
+    @DatabaseField(canBeNull = false)
+    public String password;
+
+    @DatabaseField(columnName = "fecha_hora_ultimo_inicio")
+    public String fechaHoraUltimoInicio;
+
+    @DatabaseField(canBeNull = false)
+    public String rol; // ADMIN, PRODUCTOS, ALMACENES
 }
